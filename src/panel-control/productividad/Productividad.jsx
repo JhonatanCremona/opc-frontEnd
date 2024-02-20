822c5054a18b1964c7e7cfb7eb58efb8650baab3
Jhonatan Fulguera <jfulguera@CREMONA.local>
2024-02-20 15:40:52 -0300

    Componente Productividad

--- src/panel-control/productividad/Productividad.jsx
+++ src/panel-control/productividad/Productividad.jsx
@@ -1,38 +1,44 @@
+import { useEffect, useState } from "react";
 import Style from "./Productividad.module.css";
+import JsonProductividad from "../../JSON/productivad.json"
 
 export const Productividad = () => {
-    document.addEventListener('DOMContentLoaded', function() {
-        progress_bar();
-    });
+    const [search, setSerch] = useState(false);
+
+    const dataJson = JsonProductividad;
+    let promedio;
+
+    const ciclos_correctos = (dataJson.ciclos_correctos/ dataJson.ciclos_totales) * 100;
+    console.log(ciclos_correctos);
     
     function progress_bar() {
         var speed = 30;
-        var progressBars = document.querySelectorAll(Style.progress_bar);
+        var items = document.querySelectorAll(`.${Style.progress_bar} .${Style.progress_bar_item}`);
+        console.log(items);
     
-        progressBars.forEach(function(progressBar) {
-            var items = progressBar.querySelectorAll(Style.progress_bar_item);
-
+        items.forEach(function (item) {
+            var progressBar = item.querySelector(`.${Style.progress}`);
+            var itemValue = progressBar.dataset.progress;
+            console.log(itemValue);
+            var i = 0;
+            var value = item;
     
-            items.forEach(function(item) {
-                var progress = item.querySelector(Style.progress);
-                var itemValue = parseInt(progress.getAttribute('data-progress'), 10);
-                var i = 0;
-                var value = item;
-    
-                var count = setInterval(function() {
-                    if (i <= itemValue) {
-                        var iStr = i.toString();
-                        progress.style.width = iStr + '%';
-                        value.querySelector(Style.item_value).innerHTML = iStr + '%';
-                    } else {
-                        clearInterval(count);
-                    }
-                    i++;
-                }, speed);
-            });
+            var count = setInterval(function () {
+                if (i <= itemValue) {
+                    var iStr = i.toString();
+                    progressBar.style.width = iStr + '%';
+                    value.querySelector(`.${Style.item_value}`).innerHTML = iStr + '%';
+                } else {
+                    clearInterval(count);
+                }
+                i++;
+            }, speed);
         });
     }
-    
+
+    useEffect(()=> {
+        progress_bar();
+    },[search])
 
 
     return (
@@ -40,23 +46,46 @@ export const Productividad = () => {
             <section className={Style.prod_container + " " + Style.progress_bar}>
                 <h2 className={Style.title}>Productividad</h2>
                 <article className={Style.progress_bar_item}>
-                    <h3>% CICLOS REALIZADOS CORRECTAMENTE</h3>
+                    <h3 className={Style.sub_title}>% CICLOS REALIZADOS CORRECTAMENTE</h3>
                     <div className={Style.item_bar}>
-                        <div className={Style.progress } data-progress="80"></div>
+                        <div className={Style.progress } data-progress={ ciclos_correctos }></div>
                     </div>
-                    <span className={Style.item_value}>{"87,30 %"}</span>
+                    <div className={Style.value_container}>
+                        <span className={Style.item_value}>{`0 %`}</span> 
+                        <span className={Style.item_label}>{` / ${dataJson.ciclos_totales}`}</span>
+                    </div>
                 </article>
-                <article>
-                    <h3>% USO DEL EQUIPO</h3>
-                    <div>barra de porcentaje...</div>
-                    <p><span>{"87,30 %"}</span></p>
+                <article className={Style.progress_bar_item}>
+                    <h3 className={Style.sub_title}>% USO DEL EQUIPO</h3>
+                    <div className={Style.item_bar}>
+                        <div className={Style.progress } data-progress="60"></div>
+                    </div>
+                    <span className={Style.item_value}>{"0 %"}</span>
                 </article>
                 <hr />
-                <article>
-                    <h3>% PRODUCTOS REALIZADOS</h3>
-                    <div>barra de porcentaje...</div>
-                    <p><span>{"87,30 %"}</span></p>
+                <article className={ Style.list_receta_container}>
+                    <h3 className={Style.sub_title}>% PRODUCTOS REALIZADOS</h3>
+
+                    <article className={Style.box_list_receta}>
+                        <div className={Style.list_recetas}>
+                            {dataJson.recetas.map((receta, index)=> {
+                                if (receta > 0 ) 
+                                {
+                                    promedio = Math.floor(receta/ dataJson.ciclos_correctos * 100) ;
+                                    return (
+                                    <>
+                                        <div className={Style.recetas} key={index} style={{width: `${promedio}%`}}>
+                                    {receta}
+                                    </div>
+                                    </>
+                                    )
+                                }
+                            })}
+                        </div>
+                        <span className={Style.item_label}>100%</span>
+                    </article>
                 </article>
+                
                 {" COMPONENTE "}
                 <hr />
                 <div>
